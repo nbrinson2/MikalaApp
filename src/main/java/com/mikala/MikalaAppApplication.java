c2e1a4eaedd3f147881cd20a18f23766804aac58
Nick Brinson <nickbrinson3@gmail.com>
2019-10-29 16:03:37 -0400

    First commit

---
+++ src/main/java/com/mikala/MikalaAppApplication.java
@@ -0,0 +1,13 @@
+package com.mikala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikalaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikalaAppApplication.class, args);
+	}
+
+}
